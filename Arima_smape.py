863806715c909d9f2b01b54acd58661565e17289
wangningjun <ningjun_wang@163.com>
2018-09-07 10:07:53 +0800

    Arima smape

--- Arima_smape.py
+++ Arima_smape.py
@@ -10,7 +10,7 @@ from sys import maxsize
 更新日期：2018/8/28
 说明：本脚本的内容是以ARIMA算法预测时间序列
 更新说明：实现动态调参
-准确预测未来n时刻的数据
+准确预测未来n时刻的数据,统计smape
 '''
 
 def difference(dataset, interval=1):
@@ -29,7 +29,7 @@ def Arima_up(x):
     differenced = difference(x, days_up)
     _, p, q, _ = proper_model(differenced,3)
     print(p,q)
-    model = ARIMA(differenced, order=(p, 0, q),freq='D')
+    model = ARIMA(differenced, order=(p, 0, q))
     ARIMA_model = model.fit(disp=0)
     return ARIMA_model
 
@@ -37,7 +37,7 @@ def Arima_down(x):
     differenced = difference(x, days_down)
     _,p,q,_ = proper_model(differenced,3)
     print(p,q)
-    model = ARIMA(differenced, order=(p, 0, q),freq='D')
+    model = ARIMA(differenced, order=(p, 0, q))
     ARIMA_model = model.fit(disp=0)
     return ARIMA_model
 
@@ -46,7 +46,7 @@ def predict(X,model,days):
     differenced = difference(X, days)
     start_index = len(differenced)
     end_index = len(differenced) + predict_long
-    forecast = model.predict(start=start_index, end=end_index)
+    forecast = model.predict(start=int(start_index), end=int(end_index))
     history = [x for x in X]
     # plt.plot(history, color='red', label='predict_data')
 
@@ -54,15 +54,24 @@ def predict(X,model,days):
         inverted = inverse_difference(history, yhat, days)
         if inverted<0:    # 预测出来小于0的值全都填为0
             inverted = 0
+
         history.append(inverted)
     return history
 
-def show(history):
+def show(predict,realy):
     plt.figure(figsize=(8, 4))
     plt.subplot(2, 1, 1)
-    plt.plot(history, color='red', label='predict1')
+    plt.plot(predict, color='red', label='predict')
     plt.subplot(2, 1, 2)
-    plt.plot(history, color='red', label='predict2')
+    plt.plot(realy, color='red', label='realy')
+    plt.show()
+
+def show_double(predict,realy):
+    plt.figure(figsize=(8, 4))
+    plt.subplot(2, 1, 1)
+    plt.plot(predict, color='red', label='predict')
+    plt.subplot(2, 1, 2)
+    plt.plot(realy, color='red', label='realy')
     plt.show()
 
 def proper_model(data_ts, maxLag):
@@ -88,40 +97,40 @@ def proper_model(data_ts, maxLag):
 if __name__ == '__main__':
     days_up = 24
     days_down = 24
-    predict_long = 23
+    predict_long = 29
     path_in = 'data/in/outdata/'  # 文件夹路径
     path_out = 'data/out/'
     count = 0
     for file in os.listdir(path_in):
         df = open(path_in + file, 'rb')  # file
-        # data = pd.read_csv(df, usecols = [0])
+
         data = pd.read_csv(df, header=None)
         data.columns = ['time', 'up', 'down']
-        data = data.head(136)
-        if len(data) < predict_long:
+        len_data = len(data)
+        data_ = data.head(len_data-(predict_long+1))   # 除去最后三十行 都要
+        if len(data_) < predict_long:
             continue
-        X1 = data['up']
-        X2 = data['down']
+        X1 = data_['up']
+        X2 = data_['down']
         try:
             model_up = Arima_up(X1)
         except:
             continue
-        try:
-            model_down = Arima_down(X2)
-        except:
-            continue
+        # try:
+        #     model_down = Arima_down(X2)
+        # except:
+        #     continue
         results_pre_up = predict(X1, model_up, days_up)
-        results_pre_down = predict(X2, model_down, days_down)
+        # results_pre_down = predict(X2, model_down, days_down)
         count += 1
-        if count%500 == 0:
-            print(count)
+        real_up = data['up'].tail(predict_long+1)
+        real_down = data['down'].tail(predict_long+1)
+        show(results_pre_up, real_up)
 
+        pre_up_long = np.array(results_pre_up[-(predict_long+1):])
+        smape_up = sum(abs( pre_up_long- real_up.values)/(pre_up_long+real_up.values))/(predict_long+1)*2
+        # smape_down = sum(abs(results_pre_down-real_down.values)/(results_pre_down+real_down.values))/len(results_pre_down)*2
 
-        rng = pd.date_range(data['time'][0], periods=len(data) + predict_long + 1, freq='H')
-        rng = np.array(rng)
-        results_pre = DataFrame({'time': rng, 'up': results_pre_up, 'down': results_pre_down})
-        # results_pre = pd.Series(results_pre,index=rng)
-        path = os.path.join(path_out + file)
-        results_pre.to_csv(str(path), index=0)
-        show(results_pre['down'])
-    print('total:', count)
+        print(smape_up)
+
+    # print('total:', count)
